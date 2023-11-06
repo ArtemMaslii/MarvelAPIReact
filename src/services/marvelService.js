ea85bca162adf837fda42c9a9be8d8c78877ee5f
Artem <maslijartei@gmail.com>
2023-11-06 13:28:58 +0100

    Template of the project

---
+++ src/services/marvelService.js
@@ -0,0 +1,25 @@
+
+class MarvelService {
+    _apiBase = "https://gateway.marvel.com:443/v1/public/"
+    _apiKey = "apikey=b12d377fa43785bf0b0af5c466ebcba1";
+    
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=520&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;

e82eb6fe30ecfdf410b3b7f4b8586254e7150569
Artem <maslijartei@gmail.com>
2023-12-03 14:11:53 +0100

    Add form to find character by name, made it follow user through scrolling

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,8 +4,10 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import CharSearchForm from "../charSearch/CharSearch";
 
 import decoration from '../../resources/img/vision.png';
+
 
 const MainPage = () => {
     const [selectedChar, setChar] = useState(null);
@@ -23,9 +25,14 @@ const MainPage = () => {
                 <ErrorBoundary>
                     <CharList onCharSelected={onCharSelected}/>
                 </ErrorBoundary>
-                <ErrorBoundary>
-                    <CharInfo charId={selectedChar}/>
-                </ErrorBoundary>
+                <div style={{"position": "sticky", "top": "0"}}>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharSearchForm/>
+                    </ErrorBoundary>
+                </div>
             </div>
             <img className="bg-decoration" src={decoration} alt="vision"/>
         </>
